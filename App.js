6e69c20092f3fb273cea3180096069543b6bd8f8
SYNAPSE\rajesh.kumar19 <rajesh.kumar@gigamon.com>
2023-01-17 01:14:27 +0530

    newfile

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * HMR - Hot Module Reloading
+ * File watcher algorithm
+ * Bundling
+ * Minify
+ * cleaning our code
+ * dev and production build
+ * super fast build algorithm
+ * Image optimization
+ * Caching while development
+ * compatable with older version of browser
+ * HTTPS on dev
+ * manage port number
+ * consistent Hashing Algorithm
+ * Zero config bundler
+ * 
+ * Transitive Dependencies
+ */
+import React from "react";
+// import  ReactDOM  from "react-dom/client";
+const heading = React.createElement(
+    "h1",{id:'title'},"Heading"
+)
