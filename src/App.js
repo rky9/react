f61dea4c2fbd4d4364d216fefdfda8ff751d92c2
rajesh <rajesh.coer@gmail.com>
2023-03-17 04:24:24 +0530

    slider added

--- src/App.js
+++ src/App.js
@@ -80,10 +80,6 @@ const appRouter = createBrowserRouter([
       },
     ],
   },
-  {
-    path: "/about",
-    element: <About />,
-  },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={appRouter} />);
